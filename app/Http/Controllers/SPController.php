4713f0fb083011f91aa7c3df6128caed2ee78ae6
ardilalukita14 <lukitasari623@gmail.com>
2022-09-15 14:28:48 +0700

    Change name of StandarPelayanan

--- app/Http/Controllers/SPController.php
+++ app/Http/Controllers/SPController.php
@@ -1,22 +1,22 @@
 <?php
 
 namespace App\Http\Controllers;
-use App\Models\StandarPelayanan;
+use App\Models\Pelayanan;
 use Illuminate\Http\Request;
 
 class SPController extends Controller
 {
     public function index(Request $request){
         if ($request->has('search')) { // Jika ingin melakukan pencarian judul
-            $pelayanan = StandarPelayanan::where('jenis', 'like', "%" . $request->search . "%")->paginate(5);
+            $pelayanan = Pelayanan::where('jenis', 'like', "%" . $request->search . "%")->paginate(5);
         } else { // Jika tidak melakukan pencarian judul
             //fungsi eloquent menampilkan data menggunakan pagination
-            $pelayanan = StandarPelayanan::orderBy('id', 'desc')->paginate(5); // Pagination menampilkan 5 data
+            $pelayanan = Pelayanan::orderBy('id', 'desc')->paginate(5); // Pagination menampilkan 5 data
         }
         return view('opd.pelayanan.index', compact('pelayanan'));
     }
     public function create(){
-        $data=StandarPelayanan::all();
+        $data=Pelayanan::all();
         return view('opd.pelayanan.create', compact('data'));
     }
     public function store(Request $request){
@@ -25,20 +25,20 @@ class SPController extends Controller
             'komponen' => 'required',
             'uraian' => 'required'
        ]);
-       StandarPelayanan::create($request->all());
+       Pelayanan::create($request->all());
        return redirect()->route('opd.pelayanan.index')
                         ->with('success','pelayanan Berhasil Ditambahkan!');
     }
 
     public function edit($id) {
-        $data = StandarPelayanan::all();
-        $pelayanan = StandarPelayanan::find($id);
+        $data = Pelayanan::all();
+        $pelayanan = Pelayanan::find($id);
         return view('opd.pelayanan.edit',compact('data','pelayanan'));
     }
 
     public function update(Request $request, $id)
     {
-        $pelayanan = StandarPelayanan::find($id);
+        $pelayanan = Pelayanan::find($id);
         $pelayanan->jenis = $request->jenis;
         $pelayanan->komponen = $request->komponen;
         $pelayanan->uraian= $request->uraian;
@@ -49,12 +49,12 @@ class SPController extends Controller
 
     public function show($id)
     {
-        $data= StandarPelayanan::find($id);
+        $data= Pelayanan::find($id);
         return view('opd.pelayanan.show', compact('data'));
     }
     public function destroy($id) {
         // Alert::success('pelayanan Berhasi Dihapus','Sukses');
-        StandarPelayanan::find($id)->delete();
+        Pelayanan::find($id)->delete();
         return redirect()->route('opd.pelayanan.index')
             ->with('success', 'pelayanan Berhasil Dihapus!');
     }
